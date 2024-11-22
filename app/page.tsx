bdda4684e0c99c846cb28e30a0fe569800dc1718
sabasiddique1 <sabasiddique001@gmail.com>
2024-11-22 19:39:33 +0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="center">
+      <h1 className=" text-3xl text-brand">
+        Vaultix - Only storage solution you need
+      </h1>
+    </div>
+  );
+}
